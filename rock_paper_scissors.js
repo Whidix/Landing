cf616be72c9d22bbabbe128605cb6fc521631399
Whidix <theogamer17@gmail.com>
2022-02-20 16:41:21 +0100

    adding drum

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,17 +1,22 @@
-const button = document.querySelectorAll(".stripe-w-item");
+const button = document.querySelectorAll("button");
 const scoresHTMl = document.querySelector("#scores");
+const msg = document.querySelector("#msg");
 const scores = {
   player: 0,
   computer: 0,
 };
+scoresHTMl.innerHTML = `Scores<br>Player : ${scores.player}<br>Computer : ${scores.computer}`;
 
-button.addEventListener("click", playRound);
-
-document.querySelector(
-  "#scores"
-).innerHTML = `Scores<br>Player : ${scores.player}<br>Computer : ${scores.computer}`;
+button.forEach((button) => {
+  button.addEventListener("click", () => {
+    msg.innerHTML = playRound(button.id, computerPlay());
+    scoresHTMl.innerHTML = `Scores<br>Player : ${scores.player}<br>Computer : ${scores.computer}`;
+    isWinner();
+  });
+});
 
 function computerPlay() {
+  //Generate a random selection
   switch (Math.floor(Math.random() * 3)) {
     case 0:
       return "ciseaux";
@@ -23,6 +28,7 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+  console.log(playerSelection + computerSelection);
   if (playerSelection === computerSelection) {
     return "No winners ! Both losers";
   } else if (
@@ -37,14 +43,10 @@ function playRound(playerSelection, computerSelection) {
   return "You lose";
 }
 
-/* for (let gameCount = 1; gameCount <= 5; gameCount++) {
-  const playerSelection = prompt("Pierre / Fauille / Ciseaux").toLowerCase();
-  console.log(playRound(playerSelection, computerPlay()));
+function isWinner() {
+  if (scores.player == 5) {
+    console.log("You win the game");
+  } else if (scores.computer == 5) {
+    console.log("You lose");
+  }
 }
-
-if (scores.player > scores.computer) {
-  console.log("You win the game");
-} else if (scores.player < scores.computer) {
-  console.log("You lose");
-}
-console.log("No winners in the end"); */
