cdd19f9c1500e8cae8c47c79c45bf33a297d8615
Whidix <theogamer17@gmail.com>
2022-02-22 00:31:43 +0100

    balloon update

--- balloon.js
+++ balloon.js
@@ -1,15 +1,18 @@
-const balloon = document.querySelector('#balloon');
+const balloon = document.querySelector("#balloon");
 
-window.addEventListener('keydown', inflateBalloon);
+window.addEventListener("keydown", inflateBalloon);
 
 function inflateBalloon(event) {
-    if (event.key === 'ArrowUp') {
-        balloon.style.fontSize = `${1.1*parseInt(balloon.style.fontSize)}px`;
-    } else if (event.key ==='ArrowDown') {
-        balloon.style.fontSize = `${0.9*parseInt(balloon.style.fontSize)}px`;
-    }
-    if (parseInt(balloon.style.fontSize) >= 100) {
-        balloon.innerText = '🎆';
-        window.removeEventListener('keydown', inflateBalloon);
-    }
+  if (event.key === "ArrowUp") {
+    balloon.style.fontSize = `${1.1 * parseInt(balloon.style.fontSize)}px`;
+  } else if (
+    event.key === "ArrowDown" &&
+    parseInt(balloon.style.fontSize) > 10
+  ) {
+    balloon.style.fontSize = `${0.9 * parseInt(balloon.style.fontSize)}px`;
+  }
+  if (parseInt(balloon.style.fontSize) >= 200) {
+    balloon.innerText = "💥";
+    window.removeEventListener("keydown", inflateBalloon);
+  }
 }
