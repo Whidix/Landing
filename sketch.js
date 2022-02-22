f60d72747826fc4ad2c7b59e59ce69ec4503b534
Whidix <theogamer17@gmail.com>
2022-02-22 17:49:15 +0100

    sketch almost working

--- sketch.js
+++ sketch.js
@@ -1,19 +1,31 @@
 const sketch = document.querySelector(".sketch");
-let N = 100;
-let pixel = 500 / N;
-sketch.style.width = `500px`;
-for (let x = 0; x < N * N; x++) {
-  const div = document.createElement("div");
-  div.style.width = `${pixel}px`;
-  div.style.height = `${pixel}px`;
-  sketch.append(div);
-}
+const numPixel = document.querySelector("#pixel");
+const resetButton = document.querySelector("#reset");
+
+numPixel.value = 100;
+createArea();
 
 sketch.addEventListener("mouseover", changeColor);
+numPixel.addEventListener("change", createArea);
+resetButton.addEventListener("click", createArea);
+
+function createArea(e) {
+  reset();
+  const N = numPixel.value;
+  const pixel = 500 / N;
+  for (let x = 0; x < N * N; x++) {
+    const div = document.createElement("div");
+    div.style.width = `${pixel}px`;
+    div.style.height = `${pixel}px`;
+    sketch.append(div);
+  }
+}
 
 function changeColor(e) {
   e.stopPropagation();
-  if (e.target.className === 'sketch') {return}
+  if (e.target.className === "sketch") {
+    return;
+  }
   e.target.style.backgroundColor = randomColor();
 }
 
@@ -21,4 +33,8 @@ function randomColor() {
   return `rgb(${Math.floor(Math.random() * 256)},${Math.floor(
     Math.random() * 256
   )},${Math.floor(Math.random() * 256)})`;
+}
+
+function reset() {
+  sketch.innerHTML = "";
 }
